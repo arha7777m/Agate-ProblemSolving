612348039c8f1f1a11c26c8243be1dc85b1afe46
Arhammirza Ibrahim <iarhamirzaibrahim@apps.ipb.ac.id>
2021-10-07 23:57:16 +0700

    Push Problem 4

---
+++ Assets/Scripts/PlayerMovement1.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    // Rigidbody 2D bola
+    private Rigidbody2D rigidBody2D;
+
+    // Besarnya gaya awal yang diberikan untuk mendorong bola
+    private float moveInputX;
+    private float moveInputY;
+
+    //kecepatan player
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //kalo klik R, reset ball position (keperluan debug aja)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ResetBall();
+        }
+
+        //input & bergerak move x
+        moveInputX = Input.GetAxis("Horizontal");
+        rigidBody2D.velocity = new Vector2(moveInputX * speed * Time.deltaTime, rigidBody2D.velocity.y);
+
+        //input & bergerak move y
+        moveInputY = Input.GetAxis("Vertical");
+        rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, moveInputY * speed * Time.deltaTime);
+    }
+
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        rigidBody2D.velocity = Vector2.zero;
+    }
+}
