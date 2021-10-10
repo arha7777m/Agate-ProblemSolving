19eb90ce4d69f649ecfe368e8ca10d0273c0842e
Arhammirza Ibrahim <iarhamirzaibrahim@apps.ipb.ac.id>
2021-10-10 22:54:08 +0700

    Push Problem 10 / Main

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{ 
+    public void QuitGame()
+    {
+        Debug.Log("Keluar!");
+        Application.Quit();
+    }
+
+    public void GoToScene(string nameScene)
+    {
+        SceneManager.LoadScene(nameScene);
+    }
+}
